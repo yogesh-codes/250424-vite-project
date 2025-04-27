48593e6eaf3a404c664ea3555378195ea9fa22ed
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-04-27 09:07:33 +0530

    minor

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
--- vite-project/src/components/Header/Header.tsx
+++ vite-project/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import "./Header.css";
 import UserMenu from "./UserMenu/UserMenu";

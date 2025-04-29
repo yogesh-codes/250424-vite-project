dcc7278886a6c2abd617671d22e97034173499b8
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-04-30 01:06:30 +0530

    added h3 and about-section css tags

--- vite-project/src/components/AboutPage/AboutSection/AboutSection.tsx
+++ vite-project/src/components/AboutPage/AboutSection/AboutSection.tsx
@@ -2,7 +2,10 @@ import "./AboutSection.css";
 
 function AboutSection() {
     return (
-        <article className="container-sm page" style={{ maxWidth: "35em" }}>
+        <article
+            className="about-section container-sm page"
+            style={{ maxWidth: "35em" }}
+        >
             <section className="mt-2">
                 <h2 className="">About</h2>
                 <p>This is the about section</p>
@@ -12,16 +15,49 @@ function AboutSection() {
                 <h2>Features</h2>
                 <div
                     className="d-grid gap-3"
-                    style={{ gridTemplateColumns: "1fr 1fr" }}
+                    style={{ gridTemplateColumns: "repeat(3,1fr)" }}
                 >
                     <div className="card mb-3">
                         <div className="card-body">
-                            <h3>Task Component</h3>
-                            <p>ds</p>
+                            <h3>Task CRUD</h3>
+                            <ul>
+                                <li>Create new Task</li>
+                                <li>View a task </li>
+                                <li>Delete a task</li>
+                                <li>Sort tasks</li>
+                                <li></li>
+                            </ul>
                         </div>
                     </div>
                     <div className="card mb-3">
-                        <div className="card-body"></div>
+                        <div className="card-body">
+                            <h3>Tasks Sorting</h3>
+                            <ul>
+                                <li>Sort By due date</li>
+                                <li>Sort By Alphabetical</li>
+                                <li>Sort By Project</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h3>Tasks Grouping</h3>
+                            <ul>
+                                <li>Group By due date</li>
+                                <li>Group By Project</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h3>User Authentication</h3>
+                            <ul>
+                                <li>Login (with Email Password)</li>
+                                <li>Sign up</li>
+                                <li>With Google Account</li>
+                            </ul>
+                        </div>
                     </div>
 
                     <div className="card mb-3">
